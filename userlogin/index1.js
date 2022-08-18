6fce3c9511031ec13a2db6fd6152db13c9422840
DeepthiT-131 <deepthireddy131@gmail.com>
2022-08-18 21:51:39 +0530

    firstcommit

---
+++ userlogin/index1.js
@@ -0,0 +1,63 @@
+const aws = require('aws-sdk')
+const dynamodb = new aws.DynamoDB.DocumentClient();
+exports.handler = async (event) => {
+const createAccount=async(event)=>{
+    try{
+    console.log(event)
+    var params={
+        TableName: process.env.UserTable,
+        Item: { 
+            "email_id":event.queryStringParameters.email_id,
+            "Name":event.queryStringParameters.Name, 
+            "Path":`/${event.queryStringParameters.Name}`,
+        }
+  }
+  
+    let response = await dynamodb.put(params).promise(); 
+            return true
+    }
+    
+    catch(err){
+        console.log("[ERROR]",err);
+        return{
+            statusCode:400,  
+            body:JSON.stringify({
+                "message":false, 
+                "data":err
+            })
+        }
+    }
+}
+
+const params1 = {
+    TableName : "user_details"
+}
+let res1 = await dynamodb.scan(params1).promise()
+console.log(res1.Count)     
+if (res1.Count < 5){
+   let response = await createAccount(event);
+   console.log(response);
+   return {
+            statusCode:200,
+            body:JSON.stringify({
+                "message":response,
+                "data":event.queryStringParameters
+            })
+        };
+} 
+else {
+    return {
+        statusCode:400,  
+            body:JSON.stringify({
+                "message":false,
+                "data": "User Limit exceeded"
+            })
+    }
+}
+ 
+
+}
+    
+    
+
+
---
+++ userstorage/index2.js
@@ -0,0 +1,52 @@
+const aws = require('aws-sdk');
+const s3 = new aws.S3;
+const dynamodb = new aws.DynamoDB.DocumentClient();
+exports.handler = async (event) => {
+    try{
+    const params = {
+        TableName : process.env.UserTable,
+        Key:{
+            email_id : event.queryStringParameters.email_id
+        }
+    };
+    
+    const response = await dynamodb.get(params).promise();
+    console.log("info",response);
+    
+    dynamodb.describeLimits;
+    
+    let listparams = {
+          Bucket: process.env.BucketName, 
+        
+    };
+    
+    let listres = await s3.listObjects(listparams).promise();
+    console.log("objects list",listres);
+    
+    
+    let s3params={ 
+          Body:event.body,  
+          Bucket: process.env.BucketName + response.Item.Path,  
+          Key: event.queryStringParameters.Key
+    };
+    let s3res = await s3.putObject(s3params).promise();
+    return {
+        statusCode : 200,
+        body:JSON.stringify({
+            "message": true,
+            "data":s3res
+        })
+        
+    };
+    }
+    catch(err){
+        console.log(err);
+        return {
+        statusCode : 400,
+        body:JSON.stringify({
+            "message": false,
+            "data":err
+        })
+    };
+    }
+};
